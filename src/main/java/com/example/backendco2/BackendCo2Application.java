a211418495d44f873b472f70f1e8f86567e61adc
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2025-10-29 12:10:02 -0600

    primer commit

---
+++ src/main/java/com/example/backendco2/BackendCo2Application.java
@@ -0,0 +1,13 @@
+package com.example.backendco2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendco2/BackendCo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendco2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
